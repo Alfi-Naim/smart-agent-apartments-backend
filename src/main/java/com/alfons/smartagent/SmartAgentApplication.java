0faefed53c5c73404cdc7b27fc4f9667dd5b4070
alfi-naim <alfonzapps@gmail.com>
2023-01-16 10:59:42 +0200

    initial commit

---
+++ src/main/java/com/alfons/smartagent/SmartAgentApplication.java
@@ -0,0 +1,13 @@
+package com.alfons.smartagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartAgentApplication.class, args);
+	}
+
+}
